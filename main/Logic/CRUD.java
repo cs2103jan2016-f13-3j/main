335de2c0c4e74dc79bfa37372b54878d812015dd
Kowshik Sundararajan <kowshik.sundararajan@gmail.com>
2016-02-19 17:13:23 +0800

    setup the basic architecture
    created logic, storage, parser and ui folders

---
+++ main/Logic/CRUD.java
@@ -0,0 +1,14 @@
+package Logic;
+
+public class CRUD {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		printString();
+	}
+	
+	public static void printString() {
+		System.out.println("Hello World");
+	}
+
+}
