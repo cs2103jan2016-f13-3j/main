7dd39b8caea8195f0844f14e8c96b8455e53ffbf
CG <kohchenggee@gmail.com>
2016-02-25 17:50:00 +0800

    CRUD first commit

--- main/Logic/CRUD.java
+++ main/Logic/CRUD.java
@@ -8,6 +8,28 @@ public class CRUD {
 		Storage.access.addToStorage(line);
 	}
 	
+	public static void delete(int index){
+		Storage.access.delFromStorage(index);
+	}
+	
+	public static void display(){
+		Storage.access.displayStorage();
+	}
+	
+	public static void clear(){
+		Storage.access.clear();
+	}
+	
+	public static void sort(){
+		Storage.access.sortStorage();
+	}
+	public static void search(String keyword){
+		Storage.access.searchStorage(keyword);
+	}
+	public static void saveAndExit(){
+		System.exit();
+	}
+
 	public static void displayContents() {
 		ArrayList<String> temp = new ArrayList<String>();
 		temp = Storage.access.displayStorage();
