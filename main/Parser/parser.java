74277d7f60db2a8bfdba7eab8361565b207c3b86
jk93 <jungkai3993@gmail.com>
2016-03-14 14:44:10 +0800

    made changes to add command.
    add command is able to detect date in the input if certain keywords is
    inserted before the date

--- main/Parser/parser.java
+++ main/Parser/parser.java
@@ -2,32 +2,36 @@ package Parser;
 
 import java.util.*;
 
-import Logic.crud;
+import Logic.CRUD;
 
 import java.io.*;
 import Task.Task;
 
 public class parser {
-	private static String date;
-
+	private static String date, issue;
 	private static Scanner sc = new Scanner(System.in);
-
-	private static final String EMPTY_MSG = " is empty";
-	private static final String CLEAR_MSG = "All content has been cleared.";
+	private static final String[] key = { "by", "at", "in", "on", "during" };
+	private static final String EMPTY_MSG = " Unable to delete from empty task list";
+	private static final String CLEAR_MSG = "All content deleted";
+	private static final String ADD_MSG = "is added to the task list.";
+	private static final String DELETE_MSG = "is deleted from the task list.";
 	private static final String SORT_MSG = "All items are sorted in alphabetical order";
 	private static final String EDIT_MSG = " is edited and saved";
 	private static final String MARK_MSG = " is marked as completed";
 	private static final String INVALID_MSG = "Invalid inputs! Please try again";
-	private static final String DEADLINE_MSG = "Enter deadline in dd/mm/yyyy or enter - for no deadline";
+	private static final String DEADLINE_PROMPT = "Enter deadline in dd/mm/yyyy or enter - for no deadline";
 	private static final String WRONG_DEADLINE_MSG = "Please enter deadline in dd/mm/yyyy format or enter - for no deadline";
-	private static final String SEARCH_MSG = "Press 1 to search by issue, 2 to search by date";
-	private static final String DATE_MSG = "Enter date in dd/mm/yyyy";
+	private static final String SEARCH_PROMPT = "Press 1 to search by issue, 2 to search by date";
+	private static final String ISSUE_PROMPT = "Enter keyword contained in the issue";
+	private static final String DATE_PROMPT = "Enter date in dd/mm/yyyy";
 	private static final String WRONG_DATE_MSG = "Please enter date in dd/mm/yyyy format";
+	private static final String DNE_MSG = "Task does not exists";
 
 	/**
 	 * method that simulate command line interface that will responds to user's
 	 * inputs
-	 * @throws IOException 
+	 * 
+	 * @throws IOException
 	 * 
 	 */
 	public static void run(String cmd, String description) throws IOException {
@@ -42,60 +46,75 @@ public class parser {
 	 * 
 	 * @param option
 	 * @param s
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	public static void parseCommands(String option, String s) throws IOException {
-		if(option.equals("add") || option.equals("a") || option.equals("+")) {
-			UI.ui.print(DEADLINE_MSG);
-			while (true) { //check if the user want to add date
-				date = sc.nextLine();
-				if (date.equals("-")) {
-					break;
+		if (option.equals("add") || option.equals("a") || option.equals("+")) {
+			// get index of key
+			String[] temp = s.split(" ");
+			int index = getIndexOfKey(temp);
+			date = "-";
+			if (index != -1) {// if s contains date
+				// read date
+				date = temp[index + 1];
+				if (!Logic.checkDate.checkDateformat(date)) {
+					UI.ui.print(WRONG_DATE_MSG);
+
+				} else {
+					// get issue
+					issue = getIssue(temp, index);
+					Logic.CRUD.addTask(issue, date);
+					UI.ui.print("\"" + issue + "\" " + ADD_MSG);
 				}
-				if (Logic.checkDate.checkDateformat(date)) {
-					break;
-				}
-				UI.ui.print(WRONG_DEADLINE_MSG);
-			}
-			if (date.equals("-")) {
-				Logic.crud.addTask(s);
 			} else {
-				Logic.crud.addTask(s, date);
+				Logic.CRUD.addTask(s);
+				UI.ui.print("\"" + s + "\" " + ADD_MSG);
 			}
-			UI.ui.print( "\"" + s + "\" " + "is added to the task list.");
+
 		}
 
-		else if(option.equals("delete") ||option.equals("-")) {
+		else if (option.equals("delete") || option.equals("-")) {
 			int num = Integer.parseInt(s);
-			Logic.crud.deleteTask(num - 1);
-			UI.ui.print( "\"" + s + "\" " + "is deleted from the task list.");
+			ArrayList<Task> list = Storage.localStorage.getArrayList();
+			if (list.size() == 0) {
+				UI.ui.print(EMPTY_MSG);
+			} else if (list.size() < num || num - 1 < 0) {
+				// handle indexOutofBoundException
+				UI.ui.print(DNE_MSG);
+
+			} else {
+				Task deleted = list.get(num - 1);
+				issue = deleted.getIssue();
+				Logic.CRUD.deleteTask(num - 1);
+				UI.ui.print("\"" + issue + "\" " + DELETE_MSG);
+			}
 		}
 
-		else if(option.equals("display") || option.equals("d")) {
-			Logic.crud.displayUncompletedTasks();
+		else if (option.equals("display") || option.equals("d")) {
+			Logic.CRUD.displayUncompletedTasks();
 		}
-		
-		else if(option.equals("displaycompleted") || option.equals("dc")) {
-			Logic.crud.displayCompletedTasks();
+
+		else if (option.equals("displaycompleted") || option.equals("dc")) {
+			Logic.CRUD.displayCompletedTasks();
 		}
-		
-		else if(option.equals("view") || option.equals("v")) {
+
+		else if (option.equals("view") || option.equals("v")) {
 			int num = Integer.parseInt(s);
-			Logic.crud.viewIndividualTask(num - 1);
+			Logic.CRUD.viewIndividualTask(num - 1);
 		}
 
-		else if(option.equals("clear") || option.equals("c")) {
-			Logic.crud.clearTasks();
+		else if (option.equals("clear") || option.equals("c")) {
+			Logic.CRUD.clearTasks();
 			UI.ui.print(CLEAR_MSG);
 		}
 
-		else if(option.equals("sort")) { // by alphabetical order
-			Logic.sort.sortTasksAlphabetically();
+		else if (option.equals("sort")) { // by alphabetical order
+			Logic.Sort.sortTasksAlphabetically();
 			UI.ui.print(SORT_MSG);
 		}
 
-		else if(option.equals("search") || option.equals("s")) {
-			Logic.search.searchTasksByKeyword(s);	
+		else if (option.equals("search") || option.equals("s")) {
+			Logic.Search.searchTasksByKeyword(s);
 		}
 
 		else if (option.equals("mark") || option.equals("m")) {
@@ -104,13 +123,13 @@ public class parser {
 			UI.ui.print(s + MARK_MSG);
 		}
 
-		else if(option.equals("edit") ||  option.equals("e")) {
+		else if (option.equals("edit") || option.equals("e")) {
 			int num = Integer.parseInt(s);
 			UI.ui.print("Enter edited task:");
-			String taskDescription = sc.nextLine();
+			issue = sc.nextLine();
 
 			UI.ui.print("Enter the edited date:");
-			while (true) { //check if the user want to add date
+			while (true) { // check if the user want to add date
 				date = sc.nextLine();
 				if (date.equals("-")) {
 					break;
@@ -121,25 +140,25 @@ public class parser {
 				UI.ui.print(WRONG_DEADLINE_MSG);
 			}
 			if (date.equals("-")) {
-				Logic.crud.editTask(num - 1, taskDescription);
+				Logic.CRUD.editTask(num - 1, issue);
 			} else {
-				Logic.crud.editTask(num - 1, taskDescription, date);
-			}			
+				Logic.CRUD.editTask(num - 1, issue, date);
+			}
 			UI.ui.print("Task number " + s + EDIT_MSG);
 		}
-		
-		else if(option.equals("p")) {
+
+		else if (option.equals("p")) {
 			int num = Integer.parseInt(s);
 			UI.ui.print("Enter priority");
 			String priority = sc.nextLine();
 			Logic.mark.setPriority(num - 1, priority);
 		}
 
-		else if(option.equals("exit")) {
-			Logic.crud.exit();
+		else if (option.equals("exit")) {
+			Logic.CRUD.exit();
 		}
-		
-		else if(option.equals("help")) {
+
+		else if (option.equals("help")) {
 			Logic.help.printHelpMenu();
 		}
 
@@ -148,5 +167,61 @@ public class parser {
 		}
 	}
 
+	/**
+	 * method that convert String[] to String
+	 * 
+	 * @param arr
+	 * @return String
+	 */
+	public static String arrayToString(String[] arr) {
+		String temp = Arrays.toString(arr);
+		temp = temp.substring(1, temp.length() - 1).replaceAll(", ", " ");
+		return temp;
+	}
+
+	/**
+	 * method that return the description of task from the String[] and position
+	 * of keyword derived from the user's input
+	 * 
+	 * @param arr
+	 * @param pos
+	 * @return String
+	 */
+	public static String getIssue(String[] arr, int pos) {
+		int size = arr.length - 2;
+		String[] temp = new String[size];
+		int i;
+		// copy of arr to temp without the command,keyword and date
+		for (i = 0; i < size; i++) {
+			if (i >= pos) {
+				temp[i] = arr[i + 2];
+
+			} else {
+				temp[i] = arr[i];
+			}
+		}
+		return arrayToString(temp);
+
+	}
+
+	/**
+	 * method that return the index of first keyword present in the String[] and
+	 * return -1 if no keyword is present
+	 * 
+	 * @param arr
+	 * @return Integer
+	 */
+	public static int getIndexOfKey(String[] arr) {
+		int idx = -1;
+		for (int i = 0; i < 5; i++) {
+			for (int j = 1; j < arr.length; j++) {
+				if (arr[j].equals(key[i])) {
+					idx = j;
+					break;
+				}
+			}
+		}
+		return idx;
+	}
 
 }
