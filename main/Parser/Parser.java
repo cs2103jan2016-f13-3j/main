2f2320ef3dff14853c6e6e0964aabe775a3aff0f
jk93 <jungkai3993@gmail.com>
2016-04-01 12:20:02 +0800

    Revert "fix array index error when keyword to detect end date is before keyword to detect start date"
    This reverts commit 1655f1aa5698b89161322dc77d2a6c85e97dd26e.

--- main/Parser/Parser.java
+++ main/Parser/Parser.java
@@ -16,7 +16,7 @@ public class Parser {
 	private static Scanner sc = new Scanner(System.in);
 	private static final String[] week = { "monday", "tuesday", "wednesday", "thursday", "friday", "saturday",
 	"sunday" };
-	private static final String[] key = { "by", "at", "on", "during", "before", "to","in"};
+	private static final String[] key = { "by", "at", "on", "during", "before", "to" };
 	private static final String EMPTY_MSG = "Storage is empty. Press \"add\" to add task.";
 	private static final String CLEAR_MSG = "All content deleted";
 	private static final String ADD_MSG = "is added to the task list.";
@@ -89,10 +89,7 @@ public class Parser {
 				// it
 				// has no start date
 				int end = getIndexOfKey(temp); // end has value of -1 if it has
-				// no end date
-				if (end<start) {//{ "by", "at", "on", "during", "before", "to" } is before "from"
-				  end = -1;// no end date
-				}
+				// no
 				boolean toRecurred = (temp[temp.length - 1].equals("r")); // return
 				// true
 				// if
@@ -102,7 +99,9 @@ public class Parser {
 				// set
 				// task
 				// as
-				// recurring
+				// returning
+				// end
+				// date
 				boolean isAdded;
 
 				if (!toRecurred) {
