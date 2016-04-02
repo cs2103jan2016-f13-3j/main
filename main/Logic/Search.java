6736bc40ab9a296e645d4e26bca4455611a0e9a9
Kowshik Sundararajan <kowshik.sundararajan@gmail.com>
2016-04-03 00:54:25 +0800

    fixed search one letter and search phrases

--- main/Logic/Search.java
+++ main/Logic/Search.java
@@ -24,76 +24,198 @@ public class Search {
 		UI.ui.eraseScreen();
 		searchedTasks = new ArrayList<Task>();
 		String[] searchKeywords = keyword.split(" ");
+
+
 		int counter = 0;
 		ArrayList<Task> temp = Storage.localStorage.getUncompletedTasks();
 
+		if((searchKeywords.length == 1) && (searchKeywords[0].length() == 1)) {
+			searchSingleLetter(searchKeywords, counter, temp);
+		}
+		else {
+			searchPhrase(searchKeywords, counter, temp);
+		}
+	}
+
+	public static void searchSingleLetter(String[] searchKeywords, int counter, ArrayList<Task> temp) {
+		String search = searchKeywords[0];
+		System.out.println(search.trim());
 		if(temp.size() > 0) {
 			for(int i = 0; i<temp.size(); i++) {
-				for(int j = 0; j<searchKeywords.length; j++) {
-					if(temp.get(i).getIssue().toLowerCase().contains(searchKeywords[j].toLowerCase()) || 
-							temp.get(i).getTaskString().toLowerCase().contains(searchKeywords[j].toLowerCase())) {
+				String[] taskParts = temp.get(i).getIssue().split(" ");
+				for(int j = 0; j<taskParts.length; j++) {
+					if(taskParts[j].trim().equals(search)) {
 						searchedTasks.add(temp.get(i));
 						break;
 					}
 				}
 			}
 
-			UI.ui.printGreen("UNCOMPLETED TASKS");
-			UI.ui.printGreen("Index\tStart Date\tEnd Date\tTask");
-			for(int i = 0; i<searchedTasks.size(); i++) {
-				Task temp1 = searchedTasks.get(i);
-				UI.ui.printTask(i, temp1.getStartDateString(), temp1.getEndDateString(), temp1.getIssue());
+			if(searchedTasks.size() > 0) {
+				UI.ui.printGreen("UNCOMPLETED TASKS");
+				UI.ui.printGreen("Index\tStart Date\tEnd Date\tTask");
+				for(int i = 0; i<searchedTasks.size(); i++) {
+					Task temp1 = searchedTasks.get(i);
+					UI.ui.printTask(i, temp1.getStartDateString(), temp1.getEndDateString(), temp1.getIssue());
 
-				counter++;
+					counter++;
+				}
+
+				UI.ui.print("________________________________________________________________");
+				UI.ui.print("\n");
 			}
-			
-			UI.ui.print("________________________________________________________________");
-			UI.ui.print("\n");
+		}
+		
+		temp = Storage.localStorage.getFloatingTasks();
+		if(temp.size() > 0) {
+			for(int i = 0; i<temp.size(); i++) {
+				String[] taskParts = temp.get(i).getIssue().split(" ");
+				for(int j = 0; j<taskParts.length; j++) {
+					if(taskParts[j].trim().equals(search)) {
+						System.out.println(taskParts[j]);
+						searchedTasks.add(temp.get(i));
+						break;
+					}
+				}
+			}
+
+			if(searchedTasks.size() > counter) {
+				UI.ui.printGreen("FLOATING TASKS");
+				UI.ui.printGreen("Index\tTask");
+				for(int i = counter; i<searchedTasks.size(); i++) {
+					Task temp1 = searchedTasks.get(i);
+					UI.ui.printTask(i, temp1.getStartDateString(), temp1.getEndDateString(), temp1.getIssue());
+					counter++;
+				}
+
+				UI.ui.print("________________________________________________________________");
+				UI.ui.print("\n");
+			}
+		}
+		
+		temp = Storage.localStorage.getCompletedTasks();
+		if(temp.size() > 0) {
+			for(int i = 0; i<temp.size(); i++) {
+				String[] taskParts = temp.get(i).getIssue().split(" ");
+				for(int j = 0; j<taskParts.length; j++) {
+					if(taskParts[j].trim().equals(search)) {
+						searchedTasks.add(temp.get(i));
+						break;
+					}
+				}
+			}
+
+			if(searchedTasks.size() > counter) {
+				UI.ui.printGreen("COMPLETED TASKS");
+				UI.ui.printGreen("Index\tTask");
+				for(int i = counter; i<searchedTasks.size(); i++) {
+					Task temp1 = searchedTasks.get(i);
+					UI.ui.printTask(i, temp1.getStartDateString(), temp1.getEndDateString(), temp1.getIssue());
+
+				}
+			}
+		}
+	}
+
+	public static void searchPhrase(String[] searchKeywords, int counter, ArrayList<Task> temp) {
+		
+		if(temp.size() > 0) {
+
+			for(int i = 0; i<temp.size(); i++) {
+				boolean isSuccess = true;
+				for(int j = 0; j<searchKeywords.length; j++) {
+					if(isContainsKeyword(searchKeywords, temp, i, j)) {
+						
+					}
+					else {
+						isSuccess = false;
+						break;
+					}
+				}
+				if(isSuccess) {
+					searchedTasks.add(temp.get(i));
+				}
+			}
+
+			if(searchedTasks.size() > 0) {
+				UI.ui.printGreen("UNCOMPLETED TASKS");
+				UI.ui.printGreen("Index\tStart Date\tEnd Date\tTask");
+				for(int i = 0; i<searchedTasks.size(); i++) {
+					Task temp1 = searchedTasks.get(i);
+					UI.ui.printTask(i, temp1.getStartDateString(), temp1.getEndDateString(), temp1.getIssue());
+
+					counter++;
+				}
+
+				UI.ui.print("________________________________________________________________");
+				UI.ui.print("\n");
+			}
 		}
 
 		temp = Storage.localStorage.getFloatingTasks();
 		if(temp.size() > 0) {
-
-			UI.ui.printGreen("FLOATING TASKS");
-			UI.ui.printGreen("Index\tTask");
 			for(int i = 0; i<temp.size(); i++) {
+				boolean isSuccess = true;
 				for(int j = 0; j<searchKeywords.length; j++) {
-					if(temp.get(i).getIssue().toLowerCase().contains(searchKeywords[j].toLowerCase()) || 
-							temp.get(i).getTaskString().toLowerCase().contains(searchKeywords[j].toLowerCase())) {
-						searchedTasks.add(temp.get(i));
+					if(isContainsKeyword(searchKeywords, temp, i, j)) {
+						
+					}
+					else {
+						isSuccess = false;
 						break;
 					}
 				}
+				if(isSuccess) {
+					searchedTasks.add(temp.get(i));
+				}
 			}
-		}
 
-		for(int i = counter; i<searchedTasks.size(); i++) {
-			Task temp1 = searchedTasks.get(i);
-			UI.ui.printTask(i, temp1.getStartDateString(), temp1.getEndDateString(), temp1.getIssue());
-			counter++;
+			if(searchedTasks.size() > counter) {
+				UI.ui.printGreen("FLOATING TASKS");
+				UI.ui.printGreen("Index\tTask");
+				for(int i = counter; i<searchedTasks.size(); i++) {
+					Task temp1 = searchedTasks.get(i);
+					UI.ui.printTask(i, temp1.getStartDateString(), temp1.getEndDateString(), temp1.getIssue());
+					counter++;
+				}
+
+				UI.ui.print("________________________________________________________________");
+				UI.ui.print("\n");
+			}
 		}
 
 		temp = Storage.localStorage.getCompletedTasks();
 		if(temp.size() > 0) {
-			UI.ui.print("________________________________________________________________");
-			UI.ui.print("\n");
-			UI.ui.printGreen("COMPLETED TASKS");
-			UI.ui.printGreen("Index\tTask");
 			for(int i = 0; i<temp.size(); i++) {
+				boolean isSuccess = true;
 				for(int j = 0; j<searchKeywords.length; j++) {
-					if(temp.get(i).getIssue().toLowerCase().contains(searchKeywords[j].toLowerCase()) || 
-							temp.get(i).getTaskString().toLowerCase().contains(searchKeywords[j].toLowerCase())) {
-						searchedTasks.add(temp.get(i));
+					if(isContainsKeyword(searchKeywords, temp, i, j)) {
+						
+					}
+					else {
+						isSuccess = false;
 						break;
 					}
 				}
+				if(isSuccess) {
+					searchedTasks.add(temp.get(i));
+				}
 			}
 
-			for(int i = counter; i<searchedTasks.size(); i++) {
-				Task temp1 = searchedTasks.get(i);
-				UI.ui.printTask(i, temp1.getStartDateString(), temp1.getEndDateString(), temp1.getIssue());
+			if(searchedTasks.size() > counter) {
+				UI.ui.printGreen("COMPLETED TASKS");
+				UI.ui.printGreen("Index\tTask");
+				for(int i = counter; i<searchedTasks.size(); i++) {
+					Task temp1 = searchedTasks.get(i);
+					UI.ui.printTask(i, temp1.getStartDateString(), temp1.getEndDateString(), temp1.getIssue());
 
+				}
 			}
 		}
+	}
+
+	public static boolean isContainsKeyword(String[] searchKeywords, ArrayList<Task> temp, int i, int j) {
+		return temp.get(i).getIssue().toLowerCase().contains(searchKeywords[j].toLowerCase()) || 
+				temp.get(i).getTaskString().toLowerCase().contains(searchKeywords[j].toLowerCase());
 	}
 }
