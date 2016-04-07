6309c922be7d827fc9efee306a9a4da56cb79b38
Kowshik Sundararajan <kowshik.sundararajan@gmail.com>
2016-04-08 01:53:55 +0800

    fixed error when sorting by priority

--- main/Logic/Sort.java
+++ main/Logic/Sort.java
@@ -31,7 +31,7 @@ public class Sort {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		ArrayList<Task> tempFloatingTasks = Storage.localStorage.getFloatingTasks();
 
 		for(int i = 0; i<tempFloatingTasks.size()-1; i++) {
@@ -78,7 +78,7 @@ public class Sort {
 						tempTasks.set(j, temp);
 					}
 				}
-				
+
 				else if(endDate1 == null && endDate2 != null) { //one end date is null
 					if(startDate1.compareTo(endDate2) > 0) {
 						Task temp = tempTasks.get(i);
@@ -104,13 +104,55 @@ public class Sort {
 		}
 	}
 
+	public static ArrayList<Task> sortArrayListInChronologicalOrder(ArrayList<Task> tempTasks) {
+		for(int i = 0; i<tempTasks.size(); i++) {
+			for(int j = i+1; j<tempTasks.size(); j++) {
+				Calendar startDate1 = tempTasks.get(i).getStartDate();
+				Calendar startDate2 = tempTasks.get(j).getStartDate();
+				Calendar endDate1 = tempTasks.get(i).getEndDate();
+				Calendar endDate2 = tempTasks.get(j).getEndDate();
+
+				if(endDate1 == null && endDate2 == null) { //both end dates are null
+					if(startDate1.compareTo(startDate2) > 0) {
+						Task temp = tempTasks.get(i);
+						tempTasks.set(i, tempTasks.get(j));
+						tempTasks.set(j, temp);
+					}
+				}
+				else if(endDate1 != null && endDate2 != null) { //both end dates are not null
+					if(endDate1.compareTo(endDate2) > 0) {
+						Task temp = tempTasks.get(i);
+						tempTasks.set(i, tempTasks.get(j));
+						tempTasks.set(j, temp);
+					}
+				}
+
+				else if(endDate1 == null && endDate2 != null) { //one end date is null
+					if(startDate1.compareTo(endDate2) > 0) {
+						Task temp = tempTasks.get(i);
+						tempTasks.set(i, tempTasks.get(j));
+						tempTasks.set(j, temp);
+					}
+				}
+				else {
+					if(endDate1.compareTo(startDate2) > 0) { //one end date is null
+						Task temp = tempTasks.get(i);
+						tempTasks.set(i, tempTasks.get(j));
+						tempTasks.set(j, temp);
+					}
+				}
+			}
+		}
+		return tempTasks;
+	}
+
 	/**
 	 * Function to sort tasks according to priority
 	 */
 	public static void sortTasksPriority() {
 		sortTasksChronologically();
 		ArrayList<Task> tempUncompletedTasks = Storage.localStorage.getUncompletedTasks();
-		
+
 		for(int i = 0; i<tempUncompletedTasks.size(); i++) {
 			for(int j = i+1; j<tempUncompletedTasks.size(); j++) {
 				if(tempUncompletedTasks.get(i).getPriority().equals("low") && tempUncompletedTasks.get(j).getPriority().equals("high")) {
@@ -118,13 +160,13 @@ public class Sort {
 					tempUncompletedTasks.set(i, tempUncompletedTasks.get(j));
 					tempUncompletedTasks.set(j, temp);
 				}
-				
+
 				else if(tempUncompletedTasks.get(i).getPriority().equals("medium") && tempUncompletedTasks.get(j).getPriority().equals("high")) {
 					Task temp = tempUncompletedTasks.get(i);
 					tempUncompletedTasks.set(i, tempUncompletedTasks.get(j));
 					tempUncompletedTasks.set(j, temp);
 				}
-				
+
 				else if(tempUncompletedTasks.get(i).getPriority().equals("low") && tempUncompletedTasks.get(j).getPriority().equals("medium")) {
 					Task temp = tempUncompletedTasks.get(i);
 					tempUncompletedTasks.set(i, tempUncompletedTasks.get(j));
@@ -132,40 +174,97 @@ public class Sort {
 				}
 			}
 		}
+		ArrayList<Task> highPriorityTasks = new ArrayList<Task>();
+		ArrayList<Task> mediumPriorityTasks = new ArrayList<Task>();
+		ArrayList<Task> lowPriorityTasks = new ArrayList<Task>();
+
+		for(Task t : tempUncompletedTasks) {
+			if(t.getPriority().equals("high")) {
+				highPriorityTasks.add(t);
+			}
+			else if(t.getPriority().equals("medium")) {
+				mediumPriorityTasks.add(t);
+			}
+			else if(t.getPriority().equals("low")) {
+				lowPriorityTasks.add(t);
+			}
+		}
+
+		highPriorityTasks = sortArrayListInChronologicalOrder(highPriorityTasks);
+		mediumPriorityTasks = sortArrayListInChronologicalOrder(mediumPriorityTasks);
+		lowPriorityTasks = sortArrayListInChronologicalOrder(lowPriorityTasks);
+
+		ArrayList<Task> changedTasks = new ArrayList<Task>();
+		for(Task t : highPriorityTasks) {
+			changedTasks.add(t);
+		}
+		for(Task t : mediumPriorityTasks) {
+			changedTasks.add(t);
+		}
+		for(Task t : lowPriorityTasks) {
+			changedTasks.add(t);
+		}
 		
 		try {
-			Storage.localStorage.setUncompletedTasks(tempUncompletedTasks);
+			Storage.localStorage.setUncompletedTasks(changedTasks);
 		} catch (ClassNotFoundException | IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		ArrayList<Task> tempFloatingTasks = Storage.localStorage.getFloatingTasks();
-		
-		for(int i = 0; i<tempFloatingTasks.size(); i++) {
+
+		/*for(int i = 0; i<tempFloatingTasks.size(); i++) {
 			for(int j = i+1; j<tempFloatingTasks.size(); j++) {
 				if(tempFloatingTasks.get(i).getPriority().equals("low") && tempFloatingTasks.get(j).getPriority().equals("high")) {
 					Task temp = tempFloatingTasks.get(i);
 					tempFloatingTasks.set(i, tempFloatingTasks.get(j));
 					tempFloatingTasks.set(j, temp);
 				}
-				
+
 				else if(tempFloatingTasks.get(i).getPriority().equals("medium") && tempFloatingTasks.get(j).getPriority().equals("high")) {
 					Task temp = tempFloatingTasks.get(i);
 					tempFloatingTasks.set(i, tempFloatingTasks.get(j));
 					tempFloatingTasks.set(j, temp);
 				}
-				
+
 				else if(tempFloatingTasks.get(i).getPriority().equals("low") && tempFloatingTasks.get(j).getPriority().equals("medium")) {
 					Task temp = tempFloatingTasks.get(i);
 					tempFloatingTasks.set(i, tempFloatingTasks.get(j));
 					tempFloatingTasks.set(j, temp);
 				}
 			}
+		}*/
+		
+		highPriorityTasks = new ArrayList<Task>();
+		 mediumPriorityTasks = new ArrayList<Task>();
+		lowPriorityTasks = new ArrayList<Task>();
+
+		for(Task t : tempFloatingTasks) {
+			if(t.getPriority().equals("high")) {
+				highPriorityTasks.add(t);
+			}
+			else if(t.getPriority().equals("medium")) {
+				mediumPriorityTasks.add(t);
+			}
+			else if(t.getPriority().equals("low")) {
+				lowPriorityTasks.add(t);
+			}
+		}
+
+		changedTasks = new ArrayList<Task>();
+		for(Task t : highPriorityTasks) {
+			changedTasks.add(t);
+		}
+		for(Task t : mediumPriorityTasks) {
+			changedTasks.add(t);
+		}
+		for(Task t : lowPriorityTasks) {
+			changedTasks.add(t);
 		}
 		
 		try {
-			Storage.localStorage.setFloatingTasks(tempFloatingTasks);
+			Storage.localStorage.setFloatingTasks(changedTasks);
 		} catch (ClassNotFoundException | IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
