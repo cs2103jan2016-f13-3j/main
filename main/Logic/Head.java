71fc738aee19df82c59983c700ee40cff99fa371
Tan Jie Wei <jwtan@hotmail.sg>
2016-04-10 00:50:21 +0800

    Refactored Head & Save

--- main/Logic/Head.java
+++ main/Logic/Head.java
@@ -1,75 +1,94 @@
+//@@author Cheng Gee
 package Logic;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Scanner;
 
 import org.fusesource.jansi.AnsiConsole;
 public class Head {
-	//@@author Cheng Gee
-	private static final String WELCOME_MSG_1 = "Welcome to Agendah. ";
-	private static final String WELCOME_MSG_2 = "Agendah is ready for use";
-	private static final String USER_PROMPT = "command: ";
-	private static Scanner sc = new Scanner(System.in);
+
 	private static String lastDisplay = "";
 	private static String lastDisplayArg = "";
-	private static String logo1="********   ********   ********  **       **   *****       ********   **    **";
-	private static String logo2="********   ********   ********  ***      **   **   **     ********   **    **";
-	private static String logo3="**    **   **         **        ****     **   **    **    **    **   **    **";
-	private static String logo4="**    **   **         ******    *** **   **   **     **   **    **   ********";
-	private static String logo5="********   **   ***   ******    ***  **  **   **     **   ********   ********";
-	private static String logo6="********   **    **   **        ***   ** **   **     **   ********   **    **";
-	private static String logo7="**    **   ********   ********  ***    ****   **    **    **    **   **    **";
-	private static String logo8="**    **   ********   ********  ***     ***   *******     **    **   **    **";
+
+	private static final String LOGO_LINE_1 = "********   ********   ********  **       **   *****       ********   **    **";
+	private static final String LOGO_LINE_2 = "********   ********   ********  ***      **   **   **     ********   **    **";
+	private static final String LOGO_LINE_3 = "**    **   **         **        ****     **   **    **    **    **   **    **";
+	private static final String LOGO_LINE_4 = "**    **   **         ******    *** **   **   **     **   **    **   ********";
+	private static final String LOGO_LINE_5 = "********   **   ***   ******    ***  **  **   **     **   ********   ********";
+	private static final String LOGO_LINE_6 = "********   **    **   **        ***   ** **   **     **   ********   **    **";
+	private static final String LOGO_LINE_7 = "**    **   ********   ********  ***    ****   **    **    **    **   **    **";
+	private static final String LOGO_LINE_8 = "**    **   ********   ********  ***     ***   *******     **    **   **    **";
+	private static final String USER_PROMPT = "command: ";
+	private static final String WELCOME_HELP = "Enter \"help\" for instructions.\n";
+	private static final String WELCOME_MSG_1 = "Welcome to Agendah. ";
+	private static final String WELCOME_MSG_2 = "Agendah is ready for use";
 
 	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {		
 		Logic.ImportTasks.prepareAndImportFiles();
 		AnsiConsole.systemInstall();
 
-		//		 System.out.print(ansi().eraseScreen().fgBright(RED));
-		//	checkDateAndAdd();
-		UI.ui.eraseScreen();
-		UI.ui.printGreen(logo1);
-		UI.ui.printGreen(logo2);
-		UI.ui.printGreen(logo3);
-		UI.ui.printGreen(logo4);
-		UI.ui.printGreen(logo5);
-		UI.ui.printGreen(logo6);
-		UI.ui.printGreen(logo7);
-		UI.ui.printGreen(logo8);
+		printLogo();
 
 		UI.ui.printYellow(WELCOME_MSG_1 + WELCOME_MSG_2);
 
 		UI.ui.print("\n");
-		UI.ui.printYellow("Enter \"help\" for instructions.");
+		UI.ui.printYellow(WELCOME_HELP);
 		Logic.Notification.welcomeReminder();
 		runProgram();
 	}
 
+	/**
+	 * Method to print the welcome logo of Agendah.
+	 */
+	private static void printLogo() {
+		UI.ui.eraseScreen();
+		UI.ui.printGreen(LOGO_LINE_1);
+		UI.ui.printGreen(LOGO_LINE_2);
+		UI.ui.printGreen(LOGO_LINE_3);
+		UI.ui.printGreen(LOGO_LINE_4);
+		UI.ui.printGreen(LOGO_LINE_5);
+		UI.ui.printGreen(LOGO_LINE_6);
+		UI.ui.printGreen(LOGO_LINE_7);
+		UI.ui.printGreen(LOGO_LINE_8);
+	}
+
+	/**
+	 * Main program loop.
+	 * 
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
 	public static void runProgram() throws IOException, ClassNotFoundException {
+		Save arrayListsSaver = new Save();
+
 		while (true) {
 			UI.ui.printRed(USER_PROMPT);
 			Logic.Core.acceptCommand();
+
 			lastDisplay = Parser.Parser.getCommand();
 			lastDisplayArg = Parser.Parser.getDescription();
-			
 
-
-			/*if((lastCommand.equals("sort") && (lastArg.equals("c") || lastArg.equals("chrono")))!= true) {
-
-			 }*/
 			Logic.Sort.sortTasksPriority();
-			// save all tasks into the actual file after command is done
-			Logic.Save.saveToFile();		
+
+			// save all arraylists (and their tasks) into respective files after each command is done
+			arrayListsSaver.saveToFile();		
 		}
 	}
 
-
-	//getter method
+	/**
+	 * Method to get first word of the latest executed command for keeping track of the last display view.
+	 * 
+	 * @return First word of the latest executed command.
+	 */
 	public static String getLastDisplay() {
 		return lastDisplay;
 	}
 
+	/**
+	 * Method to get arguments (if any) of the latest executed command for keeping track of the last display view.
+	 * 
+	 * @return Second word onwards (if any) of the latest executed command.
+	 */
 	public static String getLastDisplayArg() {
 		return lastDisplayArg;
 	}
--- main/Logic/Save.java
+++ main/Logic/Save.java
@@ -13,25 +13,28 @@ import com.google.gson.GsonBuilder;
 import Task.Task;
 
 public class Save {
-	
+
+	public Save() {
+	}
+
 	/**
 	 * Method to save all 3 storage arraylists into their respective save files.
 	 * 
 	 * @throws IOException
 	 */
-	public static void saveToFile() throws IOException {
+	public void saveToFile() throws IOException {
 		// Save uncompleted tasks
 		String UncompletedTasksStorageFIleName = Logic.ImportTasks.getUncompletedTasksStorageFileName();
 		saveArrayToFile(Storage.localStorage.getUncompletedTasks(), UncompletedTasksStorageFIleName);
-		
+
 		// Save completed tasks
 		String CompletedTasksStorageFileName = Logic.ImportTasks.getCompletedTasksStorageFileName();
 		saveArrayToFile(Storage.localStorage.getCompletedTasks(), CompletedTasksStorageFileName);
-		
+
 		// Save floating tasks
 		String FloatingTasksStorageFileName = Logic.ImportTasks.getFloatingTasksStorageFileName();
 		saveArrayToFile(Storage.localStorage.getFloatingTasks(), FloatingTasksStorageFileName);
-	 }
+	}
 
 	/**
 	 * Method to save the contents of an arraylist into a Gson text file.
@@ -40,7 +43,7 @@ public class Save {
 	 * @param destinationFileName The name to save the file as.
 	 * @throws IOException
 	 */
-	private static void saveArrayToFile(ArrayList<Task> sourceArray, String destinationFileName) throws IOException {
+	private void saveArrayToFile(ArrayList<Task> sourceArray, String destinationFileName) throws IOException {
 		Writer writer = new FileWriter(destinationFileName);
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 		gson.toJson(sourceArray, writer);
