69b35fcc93bf89593e86d57c55b2632b80b44f69
Kowshik Sundararajan <kowshik.sundararajan@gmail.com>
2016-04-10 23:44:44 +0800

    refactored Mark.java

--- main/Logic/Mark.java
+++ main/Logic/Mark.java
@@ -8,30 +8,30 @@ import Storage.LocalStorage;
 import Task.Task;
 
 public class Mark {
-	
+
 	private LocalStorage localStorageObject;
-	
+
+	//Constructor.
 	public Mark() {
 		localStorageObject = LocalStorage.getInstance();
 	}
 
 	/**
-	 * Function to mark tasks as completed
+	 * Function to mark tasks as completed.
 	 * 
-	 * @param index the index of the task to be marked as completed
-	 * @throws IOException 
-	 * @throws ClassNotFoundException 
+	 * @param index                  the index of the task to be marked as completed.
+	 * @throws IOException
+	 * @throws ClassNotFoundException
 	 */
 	public void markTaskAsCompleted(int index) throws IOException, ClassNotFoundException {
 		ArrayList<Task> getSize = localStorageObject.getUncompletedTasks();
 
-		if(index < getSize.size()) {
+		if (index < getSize.size()) {
 			Task temp = localStorageObject.getUncompletedTask(index);
 			temp.setComplete();
 			localStorageObject.deleteFromUncompletedTasks(index);
 			localStorageObject.addToCompletedTasks(temp);
-		}
-		else {
+		} else {
 			Task temp = localStorageObject.getFloatingTask(index - getSize.size());
 			temp.setComplete();
 			localStorageObject.deleteFromFloatingTasks(index - getSize.size());
@@ -40,16 +40,16 @@ public class Mark {
 	}
 
 	/**
-	 * Function to mark a task as uncompleted
+	 * Function to mark a task as uncompleted.
 	 * 
-	 * @param index the index of the task to be marked as uncompleted
+	 * @param index                   the index of the task to be marked as uncompleted.
 	 * @throws IOException
-	 * @throws ClassNotFoundException 
+	 * @throws ClassNotFoundException
 	 */
 	public void markTaskAsUncompleted(int index) throws IOException, ClassNotFoundException {
 		Task temp = localStorageObject.getCompletedTask(index);
 
-		if(temp.getEndDate() != null || temp.getStartDate() != null) {
+		if (temp.getEndDate() != null || temp.getStartDate() != null) {
 			localStorageObject.addToUncompletedTasks(temp);
 			localStorageObject.deleteFromCompletedTasks(index);
 		} else {
@@ -59,49 +59,64 @@ public class Mark {
 	}
 
 	/**
-	 * Function to set the priority for a task
+	 * Function to set the priority for a task.
 	 * 
-	 * @param index the index of the task to be updated
-	 * @param priority the priority to be set for the task
-	 * @throws IOException 
-	 * @throws ClassNotFoundException 
+	 * @param index                   the index of the task to be updated.
+	 * @param priority                the priority to be set for the task.
+	 * @throws IOException
+	 * @throws ClassNotFoundException
 	 */
 	public void setPriority(int index, String priority) throws ClassNotFoundException, IOException {
-		//		localStorage.copyCurrentState();
 		ArrayList<Task> getSize = localStorageObject.getUncompletedTasks();
 
-		if(index < getSize.size()) {
+		if (index < getSize.size()) {
 			Task temp = localStorageObject.getUncompletedTask(index);
 			temp.setPriority(priority);
 			localStorageObject.setUncompletedTask(index, temp);
-		}
-		else {
+		} else {
 			Task temp = localStorageObject.getFloatingTask(index - getSize.size());
 			temp.setPriority(priority);
 			localStorageObject.setFloatingTask(index - getSize.size(), temp);
 		}
 	}
 
+	/**
+	 * Function to set priority for all instances of a recurring task.
+	 * 
+	 * @param index    the index of the tasks.
+	 * @param priority the priority to be set for the task.
+	 */
 	public void setRecurringTasksPriority(int index, String priority) {
 		ArrayList<Task> tempTasks = localStorageObject.getUncompletedTasks();
 
 		Task temp = localStorageObject.getUncompletedTask(index);
 		String idOfTask = temp.getId();
 
-	if (!idOfTask.equals("")) {
-			for(int i = 0; i<tempTasks.size(); i++) {
-				if(!tempTasks.get(i).getId().equals("")) {
-					if(tempTasks.get(i).getId().equals(idOfTask)) {
+		checkIfIdMatches(priority, tempTasks, idOfTask);
+		
+		try {
+			localStorageObject.setUncompletedTasks(tempTasks);
+		} catch (ClassNotFoundException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Function to check if the id of the task matches recurring task id.
+	 * 
+	 * @param priority  the priority to be set for the task.
+	 * @param tempTasks the arraylist of tasks.
+	 * @param idOfTask  the id of the task.
+	 */
+	public void checkIfIdMatches(String priority, ArrayList<Task> tempTasks, String idOfTask) {
+		if (!idOfTask.equals("")) {
+			for (int i = 0; i < tempTasks.size(); i++) {
+				if (!tempTasks.get(i).getId().equals("")) {
+					if (tempTasks.get(i).getId().equals(idOfTask)) {
 						tempTasks.get(i).setPriority(priority);
 					}
 				}
 			}
-
-	}		
-			try {
-				localStorageObject.setUncompletedTasks(tempTasks);
-			} catch (ClassNotFoundException | IOException e) {
-				e.printStackTrace();
-			}
-	} 
+		}
+	}
 }
