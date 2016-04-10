b3788425b2ba767af268c4a47d009514d258bdd6
Jw-Tan <jwtan@hotmail.sg>
2016-04-10 15:36:34 +0800

    Commented and cleaned up checkDate

--- main/Logic/checkDate.java
+++ main/Logic/checkDate.java
@@ -1,61 +1,70 @@
 //@@author Cheng Gee
 package Logic;
+
 public class checkDate {
 
-	private static int[] leapYearDate = new int[]{31,29,31,30,31,30,31,31,30,31,30,31};
-	private static int[] commonYearDate= new int[]{31,28,31,30,31,30,31,31,30,31,30,31};
-	
+	private static final int[] LEAP_YEAR_DATE = new int[]{31,29,31,30,31,30,31,31,30,31,30,31};
+	private static final int[] COMMON_YEAR_DATE = new int[]{31,28,31,30,31,30,31,31,30,31,30,31};
+
 	/**
 	 * Function to check the format of the string if it follows the date convention
 	 * 
+	 * @param msg The string to be checked.
+	 * @return    Whether the string is a correct DD/MM/YYYY format date.
 	 */
-	public static boolean checkDateformat(String msg){
-		String[] msgArray=msg.split("/");
-		if(msgArray.length != 3 && !msg.matches("^\\d{2}/\\d{2}/\\d{4}")) {
+	public static boolean checkDateformat(String msg) {
+		String[] msgArray = msg.split("/");
+		if (msgArray.length != 3 && !msg.matches("^\\d{2}/\\d{2}/\\d{4}")) {
 			return false;
 		} else {
-			int date=Integer.parseInt(msgArray[0]);
-			int month=Integer.parseInt(msgArray[1]);
-			int year=Integer.parseInt(msgArray[2]);
-			if(month>=1 && month<=12){
-				if((year % 4) == 0 ) {
-					if(date <= leapYearDate[month-1]) {
-					return true;
+			int date = Integer.parseInt(msgArray[0]);
+			int month = Integer.parseInt(msgArray[1]);
+			int year = Integer.parseInt(msgArray[2]);
+			if (month >= 1 && month <= 12) {
+				if ((year % 4) == 0) {
+					if (date <= LEAP_YEAR_DATE[month - 1]) {
+						return true;
+					} else {
+						return false;
+					}
+				} else {
+					if (date <= COMMON_YEAR_DATE[month - 1]) {
+						return true;
+					} else {
+						return false;
+					}
 				}
-				else {
-					return false;
-				}
-			}
-			else {
-				if(date <= commonYearDate[month-1]) {
-					return true;
-				}
-				else {
-					return false;
-				}
-			}
-			}else{
+			} else {
 				return false;
 			}
 		}
 	}
-	  public static boolean checkTimeformat(String msg){
-		    String[] msgArray=msg.split(":");
-		    if(msgArray.length != 2 && !msg.matches("^\\d{2}:\\d{2}")) {
-		     return false;
-		    } else if(msg.endsWith("am")||msg.endsWith("pm")){
-		     msg=msg.substring(0, msg.length()-2);
-		     int hour=Integer.parseInt(msgArray[0]);
-		      int minute=Integer.parseInt(msgArray[1]);
-		      if(minute>=0&&minute<60&&hour>=0&&hour<=23){
-		        return true;
-		     }return false;
-		    }else {
-		     int hour=Integer.parseInt(msgArray[0]);
-		     int minute=Integer.parseInt(msgArray[1]);
-		     if(minute>=0&&minute<60&&hour>=0&&hour<=23){
-		       return true;
-		    }return false;
-		   }
-		   }
+
+	/**
+	 * Method to check a string is of a correct time format.
+	 * 
+	 * @param msg The string to be checked.
+	 * @return    Whether the string is a valid time.
+	 */
+	public static boolean checkTimeformat(String msg) {
+		String[] msgArray = msg.split(":");
+		if (msgArray.length != 2 && !msg.matches("^\\d{2}:\\d{2}")) {
+			return false;
+		} else if (msg.endsWith("am") || msg.endsWith("pm")) {
+			msg = msg.substring(0, msg.length() - 2);
+			int hour = Integer.parseInt(msgArray[0]);
+			int minute = Integer.parseInt(msgArray[1]);
+			if (minute >= 0 && minute < 60 && hour >= 0 && hour <= 23) {
+				return true;
+			}
+			return false;
+		} else {
+			int hour = Integer.parseInt(msgArray[0]);
+			int minute = Integer.parseInt(msgArray[1]);
+			if (minute >= 0 && minute < 60 && hour >= 0 && hour <= 23) {
+				return true;
+			}
+			return false;
+		}
+	}
 }
