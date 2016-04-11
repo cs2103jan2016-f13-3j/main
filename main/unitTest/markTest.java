242ae0e8c4275bbb6b57c19176933a3db3b7a760
jk93 <jungkai3993@gmail.com>
2016-04-11 16:11:47 +0800

    create unit test for mark class

---
+++ main/unitTest/markTest.java
@@ -0,0 +1,73 @@
+package unitTest;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import org.junit.Test;
+import Logic.Crud;
+import Storage.LocalStorage;
+import Task.Task;
+import Logic.Mark;
+public class markTest {
+	
+	
+	private Crud crud = Crud.getInstance();
+	private Mark mark = new Mark();
+	private String issue;
+	private ArrayList<Task> uncompleted,completed; 
+	private LocalStorage storage = LocalStorage.getInstance();
+	
+	
+	@Test
+	public void testMark() throws ClassNotFoundException, IOException {
+		issue = "task";
+		crud.clearTasks();
+		crud.addTaskWithBothDates("task", "11/04/2016", "12/04/2016", "add task ` from 11/04/2016 to 12/04/2016");
+		uncompleted = storage.getUncompletedTasks();
+		completed = storage.getCompletedTasks();
+		Task task = uncompleted.get(0);
+		assertEquals(issue,task.getIssue());
+		assertEquals(0,completed.size());
+		mark.markTaskAsCompleted(0);
+		assertEquals(0,uncompleted.size());
+		task = completed.get(0);
+		assertEquals(issue,task.getIssue());										
+		
+		
+	}
+	@Test
+	public void testUnMark() throws ClassNotFoundException, IOException {
+		issue = "task";
+		crud.clearTasks();
+		crud.addTaskWithBothDates("task", "11/04/2016", "12/04/2016", "add task ` from 11/04/2016 to 12/04/2016");
+		uncompleted = storage.getUncompletedTasks();
+		completed = storage.getCompletedTasks();
+		Task task = uncompleted.get(0);
+		assertEquals(issue,task.getIssue());
+		assertEquals(0,completed.size());
+		mark.markTaskAsCompleted(0);
+		assertEquals(0,uncompleted.size());
+		task = completed.get(0);
+		assertEquals(issue,task.getIssue());
+		mark.markTaskAsUncompleted(0);
+		task = uncompleted.get(0);
+		assertEquals(issue,task.getIssue());
+		assertEquals(0,completed.size());		
+		
+	}
+	@Test
+	public void testPriority() throws ClassNotFoundException, IOException {
+		issue = "task";
+		crud.clearTasks();
+		crud.addTaskWithBothDates("task", "11/04/2016", "12/04/2016", "add task ` from 11/04/2016 to 12/04/2016");
+		uncompleted = storage.getUncompletedTasks();
+		Task task = uncompleted.get(0);
+		assertEquals(issue,task.getIssue());
+		mark.setPriority(0,"high");
+		task = uncompleted.get(0);
+		assertEquals(issue,task.getIssue());
+		assertEquals("high",task.getPriority());
+	}
+
+}
